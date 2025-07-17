9de86bda99ed529587499edc020260a8c55fb9fd
Oleg.Chumin <oleg.tschumin@gmail.com>
2025-07-17 17:59:18 +0300

    Добавлены кнопки на первую страницу и страницы примеров

--- src/main/java/org/chumin/learning_aviator/controller/HomeController.java
+++ src/main/java/org/chumin/learning_aviator/controller/HomeController.java
@@ -16,4 +16,28 @@ public class HomeController {
         model.addAttribute("appVersion", appVersion);
         return "index";
     }
+
+    @GetMapping("/arithmetic")
+    public String showArithmeticPage(Model model) {
+        model.addAttribute("appVersion", appVersion);
+        return "examples/arithmetic";
+    }
+
+    @GetMapping("/strings")
+    public String showStringsPage(Model model) {
+        model.addAttribute("appVersion", appVersion);
+        return "examples/strings";
+    }
+
+    @GetMapping("/logic")
+    public String showLogicPage(Model model) {
+        model.addAttribute("appVersion", appVersion);
+        return "examples/logic";
+    }
+
+    @GetMapping("/dates")
+    public String showDatesPage(Model model) {
+        model.addAttribute("appVersion", appVersion);
+        return "examples/dates";
+    }
 }
